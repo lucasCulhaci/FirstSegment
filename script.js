a02c1aeb98806a137dc62c51a4733818d957a59c
Lucas Culhaci <40703191+lucasCulhaci@users.noreply.github.com>
2025-09-07 09:22:11 +0200

    1.5.9

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ willNeverPlayThisAgain.push('Crash Bandicoot N. Sane Trilogy');
 willNeverPlayThisAgain.push('Tempest');
 willNeverPlayThisAgain.push('Bad Cheese');
 willNeverPlayThisAgain.push('Black');
+willNeverPlayThisAgain.push('Terarria');
 
 // COLUMN - MY TOP PICKS
 myTopPicks.push('Hollow Knight');
